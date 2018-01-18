6159b9a5b233cf6216cd52c1db3e56c3b65b19a3
Jered Sweeney <sweeneyjered@gmail.com>
2018-01-18 00:18:58 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,13 +17,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
+    @user = User.create(username: params[:username], password: params[:password])
 
-    if !params[:username]
-      redirect '/failure'
-    else
-      User.create(username: params[:username], password: params[:password])
-      redirect '/login'
-    end
+		if @user.save
+			redirect "/login"
+		else
+			redirect "/failure"
+		end
   end
 
   get '/account' do
